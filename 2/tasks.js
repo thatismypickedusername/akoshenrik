1e4ddb8b952afd46c2d833d37491b917c8ead5e3
thatismypickedusername <44519804+thatismypickedusername@users.noreply.github.com>
2025-04-09 11:51:46 +0200

    task 2

--- 2/tasks.js
+++ 2/tasks.js
@@ -8,7 +8,7 @@
 const getBeers = require('../data')
 
 function getTargetBeer() {
-    // TODO: see task above (one person should only solve one of the tasks)
+    return getBeers[0].brewery //task2
 }
 
 console.log(getTargetBeer())
