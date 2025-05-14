8432761837f84d88f321e9dee5574b75014d926a
thatismypickedusername <44519804+thatismypickedusername@users.noreply.github.com>
2025-05-14 14:44:26 +0200

    test

--- showAll.js
+++ showAll.js
@@ -3,7 +3,7 @@
 // Feature: Show a list of all beers in a `${name} (${brewery})` format 
 // in the console.
 
-const getBeers = require('./data')
+const getBeers = require('./data');
 
 function main() {
 }
